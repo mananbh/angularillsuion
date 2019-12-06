c7070973ee541c95215980f319e1378c47ff4b28
mananbh <mananb@illusiondental.com>
2019-12-06 11:03:36 +0530

    image upload

--- src/app/views/forms/file-upload/file-upload.component.ts
+++ src/app/views/forms/file-upload/file-upload.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit,EventEmitter } from '@angular/core';
+import { Component, OnInit,EventEmitter ,ChangeDetectorRef} from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
-
+import { AppLoaderService } from '../../../shared/services/app-loader/app-loader.service';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-file-upload',
@@ -8,24 +9,61 @@ import { FileUploader } from 'ng2-file-upload';
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit {
-  public uploader: FileUploader = new FileUploader({ url: 'http://localhost:60531/api/Aligners/Upload_Files_new?fileid=88' });
+  response:string;
+  public uploader: FileUploader = new FileUploader({ url: 'http://localhost:60531/api/PP/Upload_Rx?FolderID=8',
+ formatDataFunction:async });
+
   public hasBaseDropZoneOver: boolean = false;
   console = console;
-  constructor() { 
+   constructor(private loader: AppLoaderService,private changeDetector: ChangeDetectorRef) {  
+    this.uploader.response.subscribe( res => this.response = res );
+    this.uploader.onProgressItem = (progress: any) => this.changeDetector.detectChanges();
   }
 
   ngOnInit() {
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
-    /*  this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
          console.log('FileUpload:uploaded:', item, status, response);
-         alert('File uploaded successfully');
-     };  */
-    this.uploader.onCompleteAll= () =>{
-      alert("Successfull");
-     }
+     };
+    
+
+    this.uploader.onSuccessItem = (item: any, response: any, status: any, headers: any)=> {
+      this.changeDetector.detectChanges();
+    }
+    
   }
 
-  public fileOverBase(e: any): void {
+  
+  public fileOverBase(e:any):void {
     this.hasBaseDropZoneOver = e;
   }
+
+/*   this.uploader.onBuildItemForm = (fileItem: any, form: any) => {
+    form.append('Field1', this.filedValue); //note comma separating key and value
+    form.append('Field2', this.filedValue2);
+   }; 
+   
+   this.uploader.onBuildItemForm = (fileItem: any, form: any) => {
+
+      form.append('Year' , this.year);
+
+      form.append(' Business' , this.business);
+
+      form.append('Renewal' , this.renewal);
+
+     };
+
+    this.uploader.uploadAll();
+
+    this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any): any => {
+
+   if(response){
+
+    console.log("response"+JSON.stringify(response));
+
+  }
+
+ }
+   */
+
 }
